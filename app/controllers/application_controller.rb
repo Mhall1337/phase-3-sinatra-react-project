130735faa5657c487c3e42eb667c356eb9b18914
Mitchell <Hmitchell.hall@gmail.com>
2021-12-16 14:05:20 -0600

    removed failures method from Launch class and added routes to controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,19 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/launches" do
+    launches = Launches.all
+    launches.to_json
   end
 
+  post '/launches' do
+    launches = Launches.create(params)
+    launches.to_json
+  end
+
+  delete '/launches/:id' do
+    launches = Launches.find(params[:id])
+    launches.destroy
+    launches.to_json
+  end
 end
--- app/models/launch.rb
+++ app/models/launch.rb
@@ -1,9 +1,4 @@
 class Launch < ActiveRecord::Base
     belongs_to :company
 
-    def failures
-        self[:failure_notes][0].each do |note|
-            note
-        end
-    end
 end
