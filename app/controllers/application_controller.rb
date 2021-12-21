f98c988052f90d9ccfbc276c99f3a45ecef7d621
Mitchell <Hmitchell.hall@gmail.com>
2021-12-21 11:34:52 -0600

    added serialize to initial get so launches association to its company is made

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,19 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # # Add your routes here
+def serialize(object)
+  object.to_json(
+  include: {
+    company: {
+      only: [
+        :company_name
+      ]
+    }
+  }
+)
+end
   get "/launches" do
-    launches = Launch.all
-    launches.to_json
+   serialize(Launch.all)
   end
 
   post '/launches' do
